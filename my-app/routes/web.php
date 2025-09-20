2db56d3cbaca1760713b7e45cd409b6e45a575a1
Pra7456 <prabhakarsingh745694@gmail.com>
2025-09-20 08:48:51 +0530

    new update

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -3,18 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/create', function () {
-    return view('create');
-});
-
-Route::get('/edit', function () {
-    return view('edit');
-});
-
-
-Route::get('/show', function () {
-    return view('show');
+    return view('welcome');
 });
