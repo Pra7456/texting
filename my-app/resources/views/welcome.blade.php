db015f5a551a86b51d706a713388e712f3e02630
Pra7456 <prabhakarsingh745694@gmail.com>
2025-09-22 05:20:49 +0530

    Update welcome.blade.php

--- my-app/resources/views/welcome.blade.php
+++ my-app/resources/views/welcome.blade.php
@@ -1,1 +1,78 @@
-texting....
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel curd</title>
+
+    <style>
+        h1 {
+            /* background-color: brown;
+            color: aliceblue;
+            text-align: center;
+            width: 50%;
+            */
+
+            text-align: center;
+
+        }
+
+        .heading {
+            margin: 100px;
+            border-radius: 10px;
+            background-color: blueviolet;
+            color: aliceblue;
+
+
+        }
+
+        .container {
+            border-radius: 5px;
+            border: 2px solid black;
+            margin-left: 100px;
+            margin-right: 100px;
+        }
+
+        .table {
+
+            width: 100%;
+
+        }
+
+        tbody {
+            border: 1px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <div class="heading">
+        <h1>Laravel 12 Curd</h1>
+    </div>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Date</th>
+                <th>City</th>
+            </thead>
+            <tbody>
+                <td>1</td>
+                <td>jay</td>
+                <td>10-sep-2025</td>
+                <td>delhi</td>
+            </tbody>
+        </table>
+
+    </div>
+
+
+
+</body>
+
+</html>
